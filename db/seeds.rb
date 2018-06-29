df5d5b2bbf550692152b5c37fa24d107738681be
Omar Torres <omar.torres@plated.com>
2018-06-29 10:00:01 -0400

    seed admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.destroy_all
+Admin.create(email: ENV['admin_email'], password: ENV['admin_password'])
