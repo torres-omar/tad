30e08b786e985ccc92042f85d1eca9bacf2e3145
Omar Torres <omar.torres@plated.com>
2018-07-17 18:53:14 -0400

    make gauge responsive

--- app/assets/javascripts/ui.js
+++ app/assets/javascripts/ui.js
@@ -1,23 +1,38 @@
 $(document).ready(function(){
     // disables closing of dropdown upon item clicking
     $('.dropdown-menu').on("click.bs.dropdown", function (e) { e.stopPropagation();});
-
+    updateGauge();
     // add throtle
-    $(window).resize(handleMediaChange);
-    function handleMediaChange(){
-        let gauge_a = $('#gauge-a');
-        let gauge_container_width = gauge_a.parent().width()
-        let gauge_container_height = gauge_a.parent().height()
-        let gauge_a_height = gauge_container_height 
-        let gauge_a_width = (gauge_a_height / 2) * 3
-        if(gauge_a_width > gauge_container_width){
-            gauge_a_width = gauge_container_width
-            gauge_a_height = (gauge_a_width / 3) * 2
+    $(window).resize(updateGauge);
+    function updateGauge(){
+        // gauge-inner, 0.625 of gauge-outer
+        let gauge_outer = $('.gauge-outer');
+        let gauge_inner = $('#gauge-inner');
+        let gauge_outer_under = $('#gauge-outer-under');
+        let gauge_outer_over = $('#gauge-outer-over');
+        let gauge_container = $('.gauge-container');
+        gauge_container.css('min-height', '7rem');
+        let gauge_container_width = gauge_container.width();
+        let gauge_container_height = gauge_container.height();
+        let gauge_outer_height = gauge_container_height;
+        let gauge_outer_width = (gauge_outer_height * 2);
+        if(gauge_outer_width > gauge_container_width){
+            gauge_outer_width = gauge_container_width
+            gauge_outer_height = (gauge_outer_width / 2);
+            gauge_container.css('min-height', gauge_outer_height);
         }
-        gauge_a.css('width', gauge_a_width);
-        gauge_a.css('height', gauge_a_height);
-        gauge_a.css('border-top-left-radius', gauge_a_width);
-        gauge_a.css('border-top-right-radius', gauge_a_width);
+        gauge_outer.css('width', gauge_outer_width);
+        gauge_outer.css('height', gauge_outer_height);
+        gauge_inner.css('width', gauge_outer_width * 0.625);
+        gauge_inner.css('height', gauge_outer_height * 0.625);
+        gauge_outer_under.css('border-top-left-radius', gauge_outer_width);
+        gauge_outer_under.css('border-top-right-radius', gauge_outer_width);
+        gauge_outer_over.css('border-bottom-left-radius', gauge_outer_width);
+        gauge_outer_over.css('border-bottom-right-radius', gauge_outer_width);
+        gauge_outer_over.css('top', gauge_outer_height);
+        gauge_inner.css('border-top-left-radius', gauge_outer_width);
+        gauge_inner.css('border-top-right-radius', gauge_outer_width);
+        gauge_inner.css('top', gauge_outer_height * 0.375);
     }
 });
 
