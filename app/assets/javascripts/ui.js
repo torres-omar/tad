951711d88e335eb8dd667bb49e364f543ebf77a5
Omar Torres <omar.torres@plated.com>
2018-07-17 15:41:40 -0400

    add media queries to dashboard.scss file

--- app/assets/javascripts/ui.js
+++ app/assets/javascripts/ui.js
@@ -2,19 +2,7 @@ $(document).ready(function(){
     // disables closing of dropdown upon item clicking
     $('.dropdown-menu').on("click.bs.dropdown", function (e) { e.stopPropagation();});
 
-    // trigger event only when media query is matched, not every time screen is resized
-    // let mql_sm = window.matchMedia('(max-width:  575.98px)');
-    // let mql_seg_one = window.matchMedia('(min-width: 576px) and (max-width: 767.98px)');
-    // let mql_seg_two = window.matchMedia('(min-width: 768px) and (max-width: 991.98px)');
-    // let mql_seg_three = window.matchMedia('(min-width: 992px) and (max-width: 1199.98px)');
-    // let mql_lgxl = window.matchMedia('(min-width: 1200px)');
-    // mql_sm.addListener(handleMediaChange)
-    // mql_seg_one.addListener(handleMediaChange)
-    // mql_seg_two.addListener(handleMediaChange)
-    // mql_seg_three.addListener(handleMediaChange)
-    // mql_lgxl.addListener(handleMediaChange)
-    // handleMediaChange(mql_lgxl)
-
+    // add throtle
     $(window).resize(handleMediaChange);
     function handleMediaChange(){
         let gauge_a = $('#gauge-a');
@@ -26,7 +14,6 @@ $(document).ready(function(){
             gauge_a_width = gauge_container_width
             gauge_a_height = (gauge_a_width / 3) * 2
         }
-        debugger
         gauge_a.css('width', gauge_a_width);
         gauge_a.css('height', gauge_a_height);
         gauge_a.css('border-top-left-radius', gauge_a_width);
