1e4d6a31f138c3d8057e46d091263e42de2a30b2
Omar Torres <omar.torres@plated.com>
2018-07-22 22:22:49 -0400

    refactor gauge update methods

--- app/assets/javascripts/charts/overview/offers.js
+++ app/assets/javascripts/charts/overview/offers.js
@@ -1,57 +1,53 @@
 $(document).ready(function () {
+    // graph
     let chart = Chartkick.charts["offer-acceptance-ratios-graph"]
-    // year_month
-    let year_month_gauge_outer_over = $('#gauge-outer-over_year-month');
-    let year_month_gauge_data = $('#gauge-data_year-month');
-    let year_month_offers_sent = $('#offer-acceptance-ratio_year-month--offers-sent');
-    let year_month_offers_accepted = $('#offer-acceptance-ratio_year-month--offers-accepted');
-    let year_month_date = $('#offer-acceptance-ratio_date');
-    // year
-    let year_gauge_outer_over = $('#gauge-outer-over_year');
-    let year_gauge_data = $('#gauge-data_year');
-    let year_offers_sent = $('#offer-acceptance-ratio_year--offers-sent');
-    let year_offers_accepted = $('#offer-acceptance-ratio_year--offers-accepted');
-    let year_date = $('#offer-acceptance-ratio_year');
+    // year_month gauge
+    let gauge_for_month_year = {
+        gauge_outer_over: $('#gauge-outer-over_year-month'), 
+        gauge_data: $('#gauge-data_year-month'), 
+        offers_sent: $('#offer-acceptance-ratio_year-month--offers-sent'), 
+        offers_accepted: $('#offer-acceptance-ratio_year-month--offers-accepted'), 
+        date: $('#offer-acceptance-ratio_date'),
+        form: $('#month-year-offer-acceptance-ratio_form'),
+        remote_url: '/charts/overview/month-year-offer-acceptance-ratio?'
+    }
+    // year gauge
+    let gauge_for_year = {
+        gauge_outer_over: $('#gauge-outer-over_year'),
+        gauge_data: $('#gauge-data_year'),
+        offers_sent: $('#offer-acceptance-ratio_year--offers-sent'),
+        offers_accepted: $('#offer-acceptance-ratio_year--offers-accepted'),
+        date: $('#offer-acceptance-ratio_year'),
+        form: $('#year-offer-acceptance-ratio_form'),
+        remote_url: '/charts/overview/year-offer-acceptance-ratio?'
+    }
 
+    // when graph setting are changes
     $('#offer-acceptance-ratios_submit').click(function (event) {
         event.preventDefault();
         let data = $('#offer-acceptance-ratios_form').serialize();
         chart.updateData(`/charts/overview/offer-acceptance-ratios?${data}`)
     });
 
-    $('#month-year-offer-acceptance-ratio_submit').click(function(event){
+    function handleSubmitButtonClicked(event, gauge){
         event.preventDefault();
-        let data = $('#month-year-offer-acceptance-ratio_form').serialize();
+        let data = gauge['form'].serialize();
         $.ajax({
             method: 'GET',
-            url: `/charts/overview/month-year-offer-acceptance-ratio?${data}`
-        }).then(updateYearMonthRatioGauge);
-    })
-
-    $('#year-offer-acceptance-ratio_submit').click(function(event){
-        event.preventDefault();
-        let data = $('#year-offer-acceptance-ratio_form').serialize();
-        $.ajax({
-            method: 'GET',
-            url: `/charts/overview/year-offer-acceptance-ratio?${data}`
-        }).then(updateYearRatioGauge);
-    })
-
-    function updateYearMonthRatioGauge(data){
-        let percent_turn  = data['ratio'] == 1 ? 0.5 : 0.5 * data['ratio'];
-        year_month_gauge_outer_over.css('transform', `rotate(${percent_turn}turn`);
-        year_month_gauge_data.text(data['ratio']);
-        year_month_offers_sent.text(data['offers']);
-        year_month_offers_accepted.text(data['accepted_offers']);
-        year_month_date.text(data['date']);
+            url: `${gauge['remote_url']}${data}`
+        }).then((response) => updateGauge(response, gauge));
     }
 
-    function updateYearRatioGauge(data){
-        let percent_turn = data['ratio'] == 1 ? 0.5 : 0.5 * data['ratio'];
-        year_gauge_outer_over.css('transform', `rotate(${percent_turn}turn`);
-        year_gauge_data.text(data['ratio']);
-        year_offers_sent.text(data['offers']);
-        year_offers_accepted.text(data['accepted_offers'])
-        year_date.text(data['date']);
+    function updateGauge(response, gauge){
+        let percent_turn = response['ratio'] == 1 ? 0.5 : 0.5 * response['ratio'];
+        gauge['gauge_outer_over'].css('transform', `rotate(${percent_turn}turn`);
+        gauge['gauge_data'].text(response['ratio']);
+        gauge['offers_sent'].text(response['offers']);
+        gauge['offers_accepted'].text(response['accepted_offers']);
+        gauge['date'].text(response['date']);
     }
+
+    // when gauge settings are changes
+    $('#month-year-offer-acceptance-ratio_submit').click((event) => handleSubmitButtonClicked(event, gauge_for_month_year));
+    $('#year-offer-acceptance-ratio_submit').click((event) => handleSubmitButtonClicked(event, gauge_for_year));
 })
