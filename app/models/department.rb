b15e0b490cde5411a8222a9884f40c83815f44b8
Omar Torres <omar.torres@plated.com>
2018-08-06 14:31:05 -0400

    write functions that calculate open/live jobs and open/live openings

--- app/models/department.rb
+++ app/models/department.rb
@@ -2,4 +2,48 @@ class Department < ApplicationRecord
     has_many :jobs, 
         class_name: 'Job', 
         foreign_key: :department_id
+
+    def self.get_open_jobs_for_department(department_name)
+        department = Department.find_by(name: department_name)
+        if department
+            department.jobs.where('status = ?', 'open')
+        end 
+    end
+
+    def self.get_live_jobs_for_department(department_name)
+        department = Department.find_by(name: department_name)
+        if department
+            live_jobs = [] 
+            department.jobs.includes(:job_posts).where('status = ?', 'open').each do |job| 
+                if job.job_posts.any?{ |post| post.live }
+                    live_jobs << job 
+                end
+            end
+            live_jobs
+        end
+    end
+
+    def self.get_all_openings_for_department(department_name)
+        department = Department.find_by(name: department_name)
+        if department
+            openings = []
+            department.jobs.where('status = ?', 'open').each do |job| 
+                job.openings.each{ |opening| openings << opening if opening.status == 'open' }
+            end
+            openings
+        end
+    end
+
+    def self.get_all_live_openings_for_department(department_name)
+        department = Department.find_by(name: department_name)
+        if department
+            live_openings = []
+            department.jobs.includes(:job_posts, :openings).where('status = ?', 'open').each do |job| 
+                if job.job_posts.any?{|post| post.live}
+                    job.openings.each{ |opening| live_openings << opening if opening.status == 'open'}
+                end
+            end
+            live_openings
+        end
+    end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,14 @@
 class Job < ApplicationRecord
     belongs_to :department, 
+        class_name: 'Department',
         foreign_key: :department_id,
         optional: true
+
+    has_many :openings, 
+        class_name: 'JobOpening', 
+        foreign_key: :job_id
+
+    has_many :job_posts, 
+        class_name: 'JobPost', 
+        foreign_key: :job_id
 end
