8c6129e380007d34d76c54833fee6568815c0d8e
Omar Torres <omar.torres@plated.com>
2018-08-07 13:37:08 -0400

    update guild page --success

--- app/assets/javascripts/dashboard/guilds/update_guild_page.js
+++ app/assets/javascripts/dashboard/guilds/update_guild_page.js
@@ -2,30 +2,52 @@ $(document).ready(function () {
     if ($('#dashboard_individual-guild-page').length > 0){
         var hires_years_bubbles = $("#chart-status_guild-hires-years .chart-status_bubble");
         var job_stats_bubbles = $("#chart-status_guild-job-stats .chart-status_bubble");
+        var update_notification_bar = $('#update-notification');
+        $('#notification_message').text("Update complete!");
+        update_notification_bar.css("background-color", "#FF6C36");
         // check data-updating of root component
         // if updating, activate bubbles and disable update button
         var channel = pusher.subscribe('private-tad-channel');
         channel.bind("department-update-complete", function (data) {
-            // make calls to db for updating data for each component on page (hires, live jobs/openings, jobs) 
-            // once complete, disable bubbles and activate update button
-            
             // get current guild
-            let current_guild = $.param({guild: $('#dashboard_individual-guild-page').data('guild')})
+            var current_guild = $.param({guild: $('#dashboard_individual-guild-page').data('guild')})
+            // var params = $.param({ guild: $('#dashboard_individual-guild-page').data('guild'), years: [2})
+            var displaying_notification = false
 
             $.ajax({
                 method: 'GET', 
                 url: `/charts/guilds/hires-by-year-for-guild?${current_guild}`
             }).then(function(response){
-                window.TADCharts.guilds.year_by_year_graph.updateDate(response)
+                window.TADCharts.guilds.year_by_year_graph.updateData(response)
                 hires_years_bubbles.removeClass("chart-status_bubble--active")
                 // show notification (update complete) if not already present
+                // show notification bar for x number of seconds
+                if(!displaying_notification){
+                    displaying_notification = true
+                    update_notification_bar.removeClass('notification_container--hidden');
+                    setTimeout(function () {
+                        update_notification_bar.addClass("notification_container--hidden");
+                        // enable update button
+                        // update last updated date
+                    }, 3000);
+                }
             })
 
             $.ajax({
                 method: 'GET', 
                 url: `/charts/guilds/jobs-stats-for-guild?${current_guild}`
-            }).then(function(event){
-                
+            }).then(function(response){
+                $('#guild_live-jobs').text(response.live_jobs);
+                $('#guild_live-openings').text(response.live_openings);
+                job_stats_bubbles.removeClass("chart-status_bubble--active");
+                // show notification (update complete) if not already present
+                if (!displaying_notification) {
+                    displaying_notification = true
+                    update_notification_bar.removeClass('notification_container--hidden');
+                    setTimeout(function () {
+                        update_notification_bar.addClass("notification_container--hidden");
+                    }, 3000);
+                }
             })
         })
 
@@ -52,14 +74,5 @@ $(document).ready(function () {
                 console.log('too early to update')
             }
         }
-
-    // add listener for click event
-    // event handler: 
-        // loading bubbles
-        // ajax call to method that fetches new data
-        // method first checks to see if time.now() >= last_updated + 30 min
-        // once updating of data is complete in the back
-        // send new request for each individual component (hires/year, stats, jobs)
-
     }
 });
