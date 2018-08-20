48d1b37996ef0e68aaec7e49037ca6af1e21a999
Omar Torres <omar.torres@plated.com>
2018-08-20 19:21:26 -0400

    add tooltips to columns

--- app/assets/javascripts/dashboard/guilds/recent_hires_datatable.js
+++ app/assets/javascripts/dashboard/guilds/recent_hires_datatable.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('#days-to-hire-table').DataTable({"order": []});
+    $('#days-to-hire-table').DataTable({"order": [ [5, 'desc'] ]});
     $('#days-to-hire-table_length select').addClass('custom-select days-to-hire-table_custom-select');
     $('#days-to-hire-table_filter input').addClass('form-control');
 }); 
